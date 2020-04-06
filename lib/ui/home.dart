91183811b9cc6d1a1b858d53f39e7a84605847c9
MuhammadIdees <MuhammadIdees@users.noreply.github.com>
2020-04-06 12:37:58 +0500

    Basic directory structure and signup button

---
+++ lib/ui/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:humwell_signup/ui/signup/signup.dart';
+
+class HomePage extends StatelessWidget {
+  
+  // id to set route in main
+  static String id = "/home";
+
+  /// title to be displayed in app bar
+  final String title;
+
+  HomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: RaisedButton.icon(
+          onPressed: (){
+            Navigator.pushNamed(context, SignUp.id);
+          }, 
+          icon: Icon(Icons.lock, size: 16.0), 
+          label: Text("Sign Up"),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/signup/signup.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  
+  // id to set route in main
+  static String id = "/signup";
+
+  const SignUp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
