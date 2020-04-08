2e83aac5cb579ec20be5f85c5e10bf24afe41726
MuhammadIdees <MuhammadIdees@users.noreply.github.com>
2020-04-08 15:21:39 +0500

    Added bottom buttom animation on page end
    All good :)

--- lib/ui/signup/bottom_bar.dart
+++ lib/ui/signup/bottom_bar.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:humwell_signup/ui/home.dart';
 
 class BottomBar extends StatelessWidget {
+  final bool lastPage;
+
   const BottomBar({
     Key key,
-    @required PageController pageController,
+    @required PageController pageController, this.lastPage,
   }) : _pageController = pageController, super(key: key);
 
   final PageController _pageController;
@@ -16,30 +18,41 @@ class BottomBar extends StatelessWidget {
       child: Container(
         color: Colors.transparent, 
         height: (MediaQuery.of(context).size.height / 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Stack(
+          alignment: Alignment.center,
           children: [
 
             // go to home button
-            FlatButton.icon(
-              onPressed: (){
-                Navigator.of(context).popUntil(ModalRoute.withName(HomePage.id));
-              }, 
-              icon: Icon(Icons.home), 
-              label: Text("Home"),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              alignment: lastPage? Alignment.centerRight : Alignment.centerLeft,
+              child: FlatButton.icon(
+                onPressed: (){
+                  Navigator.of(context).popUntil(ModalRoute.withName(HomePage.id));
+                }, 
+                icon: Icon(Icons.home), 
+                label: Text("Home"),
+              ),
             ),
 
             // go next page button
-            FlatButton.icon(
-              onPressed: (){
-                _pageController.animateToPage(
-                  _pageController.page.round() + 1,
-                  duration: Duration(milliseconds: 200), 
-                  curve: Curves.easeIn,
-                );
-              }, 
-              icon: Text("Skip"), 
-              label: Icon(Icons.arrow_forward_ios),
+            AnimatedOpacity(
+              duration: Duration(milliseconds: 200),
+              opacity: lastPage? 0.0 : 1.0,
+              child: Container(
+                alignment: Alignment.centerRight,
+                child: FlatButton.icon(
+                  onPressed: (){
+                    _pageController.animateToPage(
+                      _pageController.page.round() + 1,
+                      duration: Duration(milliseconds: 200), 
+                      curve: Curves.easeIn,
+                    );
+                  }, 
+                  icon: Text("Skip"), 
+                  label: Icon(Icons.arrow_forward_ios),
+                ),
+              ),
             )
           ],
         ),
--- lib/ui/signup/signup.dart
+++ lib/ui/signup/signup.dart
@@ -22,11 +22,57 @@ class _SignUpState extends State<SignUp> {
   
   // max number of pages to be displayed
   int maxPages = 5;
+  bool lastPage = false;
+  bool forward = true;
+  bool backward = true;
 
   @override
   void initState() {
     _pageController = PageController(initialPage: 0);
     
+    _pageController.addListener(() {
+      setState(() {
+        double page = _pageController.page;
+        if (page == maxPages - 1) {
+          forward = true;
+        }
+
+        if (page == maxPages - 2) {
+          forward = false;
+        }
+        
+        // if (page > maxPages - 2){
+          print("f = $forward");
+
+        if (page <= maxPages - 2 ) {
+          lastPage = false;
+        } else if (forward && page != maxPages - 1 && page > maxPages - 2) {
+          lastPage = false;
+        } else {
+          lastPage = true;
+        }
+        //   if (forward && !backward){
+        //     lastPage = false;
+        //     print("this");
+        //   }
+          
+        //   if (!forward && backward){
+        //     lastPage = true;
+        //     print("thas");
+        //   }
+        // }
+        
+        // if (page < maxPages - 1 && lastPage == true){
+        //   print("up");
+          // lastPage = _pageController.page > maxPages - 2;
+        // } else if (page > maxPages - 2){
+        //   print("down");
+        // lastPage = _pageController.page < maxPages - 1;
+        // }
+        print(lastPage);
+      });
+    });
+
     super.initState();
   }
 
@@ -60,7 +106,10 @@ class _SignUpState extends State<SignUp> {
                 maxPages: maxPages,
               ),
 
-              BottomBar(pageController: _pageController),
+              BottomBar(
+                pageController: _pageController,
+                lastPage: lastPage,
+              ),
 
             ],
           );
